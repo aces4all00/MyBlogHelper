067b1b20d4f8d7cdab54801aa3152a003df52d57
The Blind SysAdmin <aces4all00@users.noreply.github.com>
2025-02-25 11:58:38 -0800

    Add Get-BlogDate
    - Add script (ps1) file for Get-BlogDate function
    - Update README with information about added function

---
+++ Get-BlogDate.ps1
@@ -0,0 +1,32 @@
+function Get-BlogDate {
+    [CmdletBinding(
+        ConfirmImpact = 'None' 
+    )]
+    [OutputType([string])]
+    param (
+        [datetime]
+        $Date = [datetime]::UtcNow,
+
+        [ValidateSet(
+            'Full',
+            'OnlyDate',
+            'OnlyTime'
+        )]
+        [string]
+        $Mode = 'full'
+    )
+    $dtmFormat = if ($Mode -eq 'Full') {
+        'yyyy-MM-dd HH:mm zzz'
+    } elseif ($Mode -eq 'OnlyDate') {
+        'yyyy-MM-dd'
+    } elseif ($Mode -eq 'OnlyTime') {
+        'HH:mm'
+    } else {$null}
+    $tzOffsetReplace = @(
+        '([-+])(\d{2})\:(\d{2})$'
+        '$1$2$3'
+    )
+    return $Date.ToString($dtmFormat) -replace $tzOffsetReplace
+}
+
+Get-BlogDate -Mode Full
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# MyBlogHelpes
+# MyBlogHelper
  Collection of scripts to help with some of my blogging tasks.
+
+## Get-BlogDate
+
+Returns UTC date and/or time in string format(s) required by my blog. 
